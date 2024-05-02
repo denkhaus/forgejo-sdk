f12d59796580cda5e920f2b802b20cdef7493696
Bo-Yi Wu <appleboy.tw@gmail.com>
2024-05-02 06:04:51 +0000

    feat(repo): support list of repo secrets (#660)
    - Update comment to correctly describe `ListOrgActionSecretOption` as listing organization secrets instead of members
    - Add new file `repo_action.go` with functionality to list a repository's secrets including the structure and API call
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>
    
    Reviewed-on: https://gitea.com/gitea/go-sdk/pulls/660
    Reviewed-by: Lunny Xiao <xiaolunwen@gmail.com>
    Co-authored-by: Bo-Yi Wu <appleboy.tw@gmail.com>
    Co-committed-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- forgejo/repo_action.go
+++ forgejo/repo_action.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-// Copyright 2023 The Gitea Authors. All rights reserved.
+// Copyright 2024 The Gitea Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
@@ -13,20 +13,20 @@ import (
 	"net/url"
 )
 
-// ListOrgActionSecretOption list OrgActionSecret options
-type ListOrgActionSecretOption struct {
+// ListRepoActionSecretOption list RepoActionSecret options
+type ListRepoActionSecretOption struct {
 	ListOptions
 }
 
-// ListOrgActionSecret list an organization's secrets
-func (c *Client) ListOrgActionSecret(org string, opt ListOrgActionSecretOption) ([]*Secret, *Response, error) {
-	if err := escapeValidatePathSegments(&org); err != nil {
+// ListRepoActionSecret list a repository's secrets
+func (c *Client) ListRepoActionSecret(user, repo string, opt ListRepoActionSecretOption) ([]*Secret, *Response, error) {
+	if err := escapeValidatePathSegments(&user, &repo); err != nil {
 		return nil, nil, err
 	}
 	opt.setDefaults()
 	secrets := make([]*Secret, 0, opt.PageSize)
 
-	link, _ := url.Parse(fmt.Sprintf("/orgs/%s/actions/secrets", org))
+	link, _ := url.Parse(fmt.Sprintf("/repos/%s/%s/actions/secrets", user, repo))
 	link.RawQuery = opt.getURLQuery().Encode()
 	resp, err := c.getParsedResponse("GET", link.String(), jsonHeader, nil, &secrets)
 	return secrets, resp, err
