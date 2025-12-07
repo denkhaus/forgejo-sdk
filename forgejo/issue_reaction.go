c68e3f860ff7c610d469f0a8cd7b40bb3654f813
Martijn van der Kleijn <martijn.niji@gmail.com>
2025-12-08 00:04:34 +0100

    chore: migrate reaction to generated model

--- forgejo/issue_reaction.go
+++ forgejo/issue_reaction.go
@@ -12,34 +12,26 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"time"
 
 	"codeberg.org/mvdkleijn/forgejo-sdk/forgejo/v2/models"
 )
 
-// Reaction contain one reaction
-type Reaction struct {
-	User     *models.User `json:"user"`
-	Reaction string       `json:"content"`
-	Created  time.Time    `json:"created_at"`
-}
-
 // GetIssueReactions get a list reactions of an issue
-func (c *Client) GetIssueReactions(owner, repo string, index int64) ([]*Reaction, *Response, error) {
+func (c *Client) GetIssueReactions(owner, repo string, index int64) ([]*models.Reaction, *Response, error) {
 	if err := escapeValidatePathSegments(&owner, &repo); err != nil {
 		return nil, nil, err
 	}
-	reactions := make([]*Reaction, 0, 10)
+	reactions := make([]*models.Reaction, 0, 10)
 	resp, err := c.getParsedResponse("GET", fmt.Sprintf("/repos/%s/%s/issues/%d/reactions", owner, repo, index), nil, nil, &reactions)
 	return reactions, resp, err
 }
 
 // GetIssueCommentReactions get a list of reactions from a comment of an issue
-func (c *Client) GetIssueCommentReactions(owner, repo string, commentID int64) ([]*Reaction, *Response, error) {
+func (c *Client) GetIssueCommentReactions(owner, repo string, commentID int64) ([]*models.Reaction, *Response, error) {
 	if err := escapeValidatePathSegments(&owner, &repo); err != nil {
 		return nil, nil, err
 	}
-	reactions := make([]*Reaction, 0, 10)
+	reactions := make([]*models.Reaction, 0, 10)
 	resp, err := c.getParsedResponse("GET", fmt.Sprintf("/repos/%s/%s/issues/comments/%d/reactions", owner, repo, commentID), nil, nil, &reactions)
 	return reactions, resp, err
 }
@@ -50,11 +42,11 @@ type editReactionOption struct {
 }
 
 // PostIssueReaction add a reaction to an issue
-func (c *Client) PostIssueReaction(owner, repo string, index int64, reaction string) (*Reaction, *Response, error) {
+func (c *Client) PostIssueReaction(owner, repo string, index int64, reaction string) (*models.Reaction, *Response, error) {
 	if err := escapeValidatePathSegments(&owner, &repo); err != nil {
 		return nil, nil, err
 	}
-	reactionResponse := new(Reaction)
+	reactionResponse := new(models.Reaction)
 	body, err := json.Marshal(&editReactionOption{Reaction: reaction})
 	if err != nil {
 		return nil, nil, err
@@ -79,11 +71,11 @@ func (c *Client) DeleteIssueReaction(owner, repo string, index int64, reaction s
 }
 
 // PostIssueCommentReaction add a reaction to a comment of an issue
-func (c *Client) PostIssueCommentReaction(owner, repo string, commentID int64, reaction string) (*Reaction, *Response, error) {
+func (c *Client) PostIssueCommentReaction(owner, repo string, commentID int64, reaction string) (*models.Reaction, *Response, error) {
 	if err := escapeValidatePathSegments(&owner, &repo); err != nil {
 		return nil, nil, err
 	}
-	reactionResponse := new(Reaction)
+	reactionResponse := new(models.Reaction)
 	body, err := json.Marshal(&editReactionOption{Reaction: reaction})
 	if err != nil {
 		return nil, nil, err
