3c63770a5cd533da6b20deb9e7307e6706665628
Martijn van der Kleijn <martijn.niji@gmail.com>
2025-12-07 23:32:23 +0100

    chore: migrate orgmember and orgpermissions to generated ones

--- forgejo/org_member.go
+++ forgejo/org_member.go
@@ -124,22 +124,22 @@ func (c *Client) SetPublicOrgMembership(org, user string, visible bool) (*Respon
 	}
 }
 
-// OrgPermissions represents the permissions for an user in an organization
-type OrgPermissions struct {
-	CanCreateRepository bool `json:"can_create_repository"`
-	CanRead             bool `json:"can_read"`
-	CanWrite            bool `json:"can_write"`
-	IsAdmin             bool `json:"is_admin"`
-	IsOwner             bool `json:"is_owner"`
-}
+// // OrgPermissions represents the permissions for an user in an organization
+// type OrgPermissions struct {
+// 	CanCreateRepository bool `json:"can_create_repository"`
+// 	CanRead             bool `json:"can_read"`
+// 	CanWrite            bool `json:"can_write"`
+// 	IsAdmin             bool `json:"is_admin"`
+// 	IsOwner             bool `json:"is_owner"`
+// }
 
 // GetOrgPermissions returns user permissions for specific organization.
-func (c *Client) GetOrgPermissions(org, user string) (*OrgPermissions, *Response, error) {
+func (c *Client) GetOrgPermissions(org, user string) (*models.OrganizationPermissions, *Response, error) {
 	if err := escapeValidatePathSegments(&org, &user); err != nil {
 		return nil, nil, err
 	}
 
-	perm := &OrgPermissions{}
+	perm := &models.OrganizationPermissions{}
 	resp, err := c.getParsedResponse("GET", fmt.Sprintf("/users/%s/orgs/%s/permissions", user, org), jsonHeader, nil, &perm)
 	if err != nil {
 		return nil, resp, err
