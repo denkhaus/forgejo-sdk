cc4efad315cee33078fe201075ebb218993ffd9b
Martijn van der Kleijn <martijn.niji@gmail.com>
2025-12-07 18:20:55 +0100

    chore: replace gitentry and gittreeresponse with generated models

--- forgejo/repo_tree.go
+++ forgejo/repo_tree.go
@@ -10,27 +10,9 @@ package forgejo
 
 import (
 	"fmt"
+
+	"codeberg.org/mvdkleijn/forgejo-sdk/forgejo/v2/models"
 )
-
-// GitEntry represents a git tree
-type GitEntry struct {
-	Path string `json:"path"`
-	Mode string `json:"mode"`
-	Type string `json:"type"`
-	Size int64  `json:"size"`
-	SHA  string `json:"sha"`
-	URL  string `json:"url"`
-}
-
-// GitTreeResponse returns a git tree
-type GitTreeResponse struct {
-	SHA        string     `json:"sha"`
-	URL        string     `json:"url"`
-	Entries    []GitEntry `json:"tree"`
-	Truncated  bool       `json:"truncated"`
-	Page       int        `json:"page"`
-	TotalCount int        `json:"total_count"`
-}
 
 // GetTreesOptions options for listing a repository's trees
 type GetTreesOptions struct {
@@ -40,13 +22,13 @@ type GetTreesOptions struct {
 
 // GetTrees downloads a file of repository, ref can be branch/tag/commit.
 // e.g.: ref -> main, tree -> macaron.go(no leading slash)
-func (c *Client) GetTrees(user, repo, ref string, opt GetTreesOptions) (*GitTreeResponse, *Response, error) {
+func (c *Client) GetTrees(user, repo, ref string, opt GetTreesOptions) (*models.GitTreeResponse, *Response, error) {
 	if err := escapeValidatePathSegments(&user, &repo, &ref); err != nil {
 		return nil, nil, err
 	}
 	opt.setDefaults()
 
-	trees := new(GitTreeResponse)
+	trees := new(models.GitTreeResponse)
 	recInt := 0
 	if opt.Recursive {
 		recInt = 1
