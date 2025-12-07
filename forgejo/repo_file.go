2e02f5573ad087763c48abca43d7b0d72da269bd
Martijn van der Kleijn <martijn.niji@gmail.com>
2025-12-07 21:56:18 +0100

    chore: replace repo file related models with generated ones

--- forgejo/repo_file.go
+++ forgejo/repo_file.go
@@ -16,6 +16,8 @@ import (
 	"io"
 	"net/url"
 	"strings"
+
+	"codeberg.org/mvdkleijn/forgejo-sdk/forgejo/v2/models"
 )
 
 // FileOptions options for all file APIs
@@ -66,61 +68,6 @@ type UpdateFileOptions struct {
 	FromPath string `json:"from_path"`
 }
 
-// FileLinksResponse contains the links for a repo's file
-type FileLinksResponse struct {
-	Self    *string `json:"self"`
-	GitURL  *string `json:"git"`
-	HTMLURL *string `json:"html"`
-}
-
-// ContentsResponse contains information about a repo's entry's (dir, file, symlink, submodule) metadata and content
-type ContentsResponse struct {
-	Name string `json:"name"`
-	Path string `json:"path"`
-	SHA  string `json:"sha"`
-	// `type` will be `file`, `dir`, `symlink`, or `submodule`
-	Type string `json:"type"`
-	Size int64  `json:"size"`
-	// `encoding` is populated when `type` is `file`, otherwise null
-	Encoding *string `json:"encoding"`
-	// `content` is populated when `type` is `file`, otherwise null
-	Content *string `json:"content"`
-	// `target` is populated when `type` is `symlink`, otherwise null
-	Target      *string `json:"target"`
-	URL         *string `json:"url"`
-	HTMLURL     *string `json:"html_url"`
-	GitURL      *string `json:"git_url"`
-	DownloadURL *string `json:"download_url"`
-	// `submodule_git_url` is populated when `type` is `submodule`, otherwise null
-	SubmoduleGitURL *string            `json:"submodule_git_url"`
-	Links           *FileLinksResponse `json:"_links"`
-}
-
-// FileCommitResponse contains information generated from a Git commit for a repo's file.
-type FileCommitResponse struct {
-	CommitMeta
-	HTMLURL   string        `json:"html_url"`
-	Author    *CommitUser   `json:"author"`
-	Committer *CommitUser   `json:"committer"`
-	Parents   []*CommitMeta `json:"parents"`
-	Message   string        `json:"message"`
-	Tree      *CommitMeta   `json:"tree"`
-}
-
-// FileResponse contains information about a repo's file
-type FileResponse struct {
-	Content      *ContentsResponse          `json:"content"`
-	Commit       *FileCommitResponse        `json:"commit"`
-	Verification *PayloadCommitVerification `json:"verification"`
-}
-
-// FileDeleteResponse contains information about a repo's file that was deleted
-type FileDeleteResponse struct {
-	Content      interface{}                `json:"content"` // to be set to nil
-	Commit       *FileCommitResponse        `json:"commit"`
-	Verification *PayloadCommitVerification `json:"verification"`
-}
-
 // GetFile downloads a file of repository, ref can be branch/tag/commit.
 // it optional can resolve lfs pointers and server the file instead
 // e.g.: ref -> master, filepath -> README.md (no leading slash)
@@ -166,12 +113,12 @@ func (c *Client) GetFileReader(owner, repo, ref, filepath string, resolveLFS ...
 
 // GetContents get the metadata and contents of a file in a repository
 // ref is optional
-func (c *Client) GetContents(owner, repo, ref, filepath string) (*ContentsResponse, *Response, error) {
+func (c *Client) GetContents(owner, repo, ref, filepath string) (*models.ContentsResponse, *Response, error) {
 	data, resp, err := c.getDirOrFileContents(owner, repo, ref, filepath)
 	if err != nil {
 		return nil, resp, err
 	}
-	cr := new(ContentsResponse)
+	cr := new(models.ContentsResponse)
 	if json.Unmarshal(data, &cr) != nil {
 		return nil, resp, fmt.Errorf("expect file, got directory")
 	}
@@ -180,12 +127,12 @@ func (c *Client) GetContents(owner, repo, ref, filepath string) (*ContentsRespon
 
 // ListContents gets a list of entries in a dir
 // ref is optional
-func (c *Client) ListContents(owner, repo, ref, filepath string) ([]*ContentsResponse, *Response, error) {
+func (c *Client) ListContents(owner, repo, ref, filepath string) ([]*models.ContentsResponse, *Response, error) {
 	data, resp, err := c.getDirOrFileContents(owner, repo, ref, filepath)
 	if err != nil {
 		return nil, resp, err
 	}
-	crl := make([]*ContentsResponse, 0)
+	crl := make([]*models.ContentsResponse, 0)
 	if json.Unmarshal(data, &crl) != nil {
 		return nil, resp, fmt.Errorf("expect directory, got file")
 	}
@@ -201,7 +148,7 @@ func (c *Client) getDirOrFileContents(owner, repo, ref, filepath string) ([]byte
 }
 
 // CreateFile create a file in a repository
-func (c *Client) CreateFile(owner, repo, filepath string, opt CreateFileOptions) (*FileResponse, *Response, error) {
+func (c *Client) CreateFile(owner, repo, filepath string, opt CreateFileOptions) (*models.FileResponse, *Response, error) {
 	var err error
 	if opt.BranchName, err = c.setDefaultBranchForOldVersions(owner, repo, opt.BranchName); err != nil {
 		return nil, nil, err
@@ -215,13 +162,13 @@ func (c *Client) CreateFile(owner, repo, filepath string, opt CreateFileOptions)
 	if err != nil {
 		return nil, nil, err
 	}
-	fr := new(FileResponse)
+	fr := new(models.FileResponse)
 	resp, err := c.getParsedResponse("POST", fmt.Sprintf("/repos/%s/%s/contents/%s", owner, repo, filepath), jsonHeader, bytes.NewReader(body), fr)
 	return fr, resp, err
 }
 
 // UpdateFile update a file in a repository
-func (c *Client) UpdateFile(owner, repo, filepath string, opt UpdateFileOptions) (*FileResponse, *Response, error) {
+func (c *Client) UpdateFile(owner, repo, filepath string, opt UpdateFileOptions) (*models.FileResponse, *Response, error) {
 	var err error
 	if opt.BranchName, err = c.setDefaultBranchForOldVersions(owner, repo, opt.BranchName); err != nil {
 		return nil, nil, err
@@ -236,7 +183,7 @@ func (c *Client) UpdateFile(owner, repo, filepath string, opt UpdateFileOptions)
 	if err != nil {
 		return nil, nil, err
 	}
-	fr := new(FileResponse)
+	fr := new(models.FileResponse)
 	resp, err := c.getParsedResponse("PUT", fmt.Sprintf("/repos/%s/%s/contents/%s", owner, repo, filepath), jsonHeader, bytes.NewReader(body), fr)
 	return fr, resp, err
 }
