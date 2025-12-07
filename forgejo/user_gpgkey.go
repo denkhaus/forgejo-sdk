6333918c51c259a5754c466fbf69a3905fdb92d6
Martijn van der Kleijn <martijn.niji@gmail.com>
2025-12-07 17:36:11 +0100

    chore: replace manual gpgkey model with generated version

--- forgejo/user_gpgkey.go
+++ forgejo/user_gpgkey.go
@@ -12,24 +12,25 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"time"
+
+	"codeberg.org/mvdkleijn/forgejo-sdk/forgejo/v2/models"
 )
 
-// GPGKey a user GPG key to sign commit and tag in repository
-type GPGKey struct {
-	ID                int64          `json:"id"`
-	PrimaryKeyID      string         `json:"primary_key_id"`
-	KeyID             string         `json:"key_id"`
-	PublicKey         string         `json:"public_key"`
-	Emails            []*GPGKeyEmail `json:"emails"`
-	SubsKey           []*GPGKey      `json:"subkeys"`
-	CanSign           bool           `json:"can_sign"`
-	CanEncryptComms   bool           `json:"can_encrypt_comms"`
-	CanEncryptStorage bool           `json:"can_encrypt_storage"`
-	CanCertify        bool           `json:"can_certify"`
-	Created           time.Time      `json:"created_at,omitempty"`
-	Expires           time.Time      `json:"expires_at,omitempty"`
-}
+// // GPGKey a user GPG key to sign commit and tag in repository
+// type GPGKey struct {
+// 	ID                int64          `json:"id"`
+// 	PrimaryKeyID      string         `json:"primary_key_id"`
+// 	KeyID             string         `json:"key_id"`
+// 	PublicKey         string         `json:"public_key"`
+// 	Emails            []*GPGKeyEmail `json:"emails"`
+// 	SubsKey           []*GPGKey      `json:"subkeys"`
+// 	CanSign           bool           `json:"can_sign"`
+// 	CanEncryptComms   bool           `json:"can_encrypt_comms"`
+// 	CanEncryptStorage bool           `json:"can_encrypt_storage"`
+// 	CanCertify        bool           `json:"can_certify"`
+// 	Created           time.Time      `json:"created_at,omitempty"`
+// 	Expires           time.Time      `json:"expires_at,omitempty"`
+// }
 
 // GPGKeyEmail an email attached to a GPGKey
 type GPGKeyEmail struct {
@@ -43,27 +44,27 @@ type ListGPGKeysOptions struct {
 }
 
 // ListGPGKeys list all the GPG keys of the user
-func (c *Client) ListGPGKeys(user string, opt ListGPGKeysOptions) ([]*GPGKey, *Response, error) {
+func (c *Client) ListGPGKeys(user string, opt ListGPGKeysOptions) ([]*models.GPGKey, *Response, error) {
 	if err := escapeValidatePathSegments(&user); err != nil {
 		return nil, nil, err
 	}
 	opt.setDefaults()
-	keys := make([]*GPGKey, 0, opt.PageSize)
+	keys := make([]*models.GPGKey, 0, opt.PageSize)
 	resp, err := c.getParsedResponse("GET", fmt.Sprintf("/users/%s/gpg_keys?%s", user, opt.getURLQuery().Encode()), nil, nil, &keys)
 	return keys, resp, err
 }
 
 // ListMyGPGKeys list all the GPG keys of current user
-func (c *Client) ListMyGPGKeys(opt *ListGPGKeysOptions) ([]*GPGKey, *Response, error) {
+func (c *Client) ListMyGPGKeys(opt *ListGPGKeysOptions) ([]*models.GPGKey, *Response, error) {
 	opt.setDefaults()
-	keys := make([]*GPGKey, 0, opt.PageSize)
+	keys := make([]*models.GPGKey, 0, opt.PageSize)
 	resp, err := c.getParsedResponse("GET", fmt.Sprintf("/user/gpg_keys?%s", opt.getURLQuery().Encode()), nil, nil, &keys)
 	return keys, resp, err
 }
 
 // GetGPGKey get current user's GPG key by key id
-func (c *Client) GetGPGKey(keyID int64) (*GPGKey, *Response, error) {
-	key := new(GPGKey)
+func (c *Client) GetGPGKey(keyID int64) (*models.GPGKey, *Response, error) {
+	key := new(models.GPGKey)
 	resp, err := c.getParsedResponse("GET", fmt.Sprintf("/user/gpg_keys/%d", keyID), nil, nil, &key)
 	return key, resp, err
 }
@@ -76,12 +77,12 @@ type CreateGPGKeyOption struct {
 }
 
 // CreateGPGKey create GPG key with options
-func (c *Client) CreateGPGKey(opt CreateGPGKeyOption) (*GPGKey, *Response, error) {
+func (c *Client) CreateGPGKey(opt CreateGPGKeyOption) (*models.GPGKey, *Response, error) {
 	body, err := json.Marshal(&opt)
 	if err != nil {
 		return nil, nil, err
 	}
-	key := new(GPGKey)
+	key := new(models.GPGKey)
 	resp, err := c.getParsedResponse("POST", "/user/gpg_keys", jsonHeader, bytes.NewReader(body), key)
 	return key, resp, err
 }
--- forgejo/user_key.go
+++ forgejo/user_key.go
@@ -16,19 +16,6 @@ import (
 	"codeberg.org/mvdkleijn/forgejo-sdk/forgejo/v2/models"
 )
 
-// // PublicKey publickey is a user key to push code to repository
-// type PublicKey struct {
-// 	ID          int64        `json:"id"`
-// 	Key         string       `json:"key"`
-// 	URL         string       `json:"url,omitempty"`
-// 	Title       string       `json:"title,omitempty"`
-// 	Fingerprint string       `json:"fingerprint,omitempty"`
-// 	Created     time.Time    `json:"created_at,omitempty"`
-// 	Owner       *models.User `json:"user,omitempty"`
-// 	ReadOnly    bool         `json:"read_only,omitempty"`
-// 	KeyType     string       `json:"key_type,omitempty"`
-// }
-
 // ListPublicKeysOptions options for listing a user's PublicKeys
 type ListPublicKeysOptions struct {
 	ListOptions
