07a3b523d8b610349fdb319ae1edbaf5e3c5d512
Martijn van der Kleijn <martijn.niji@gmail.com>
2025-12-08 01:01:01 +0100

    chore: migrate to generated cron model

--- forgejo/admin_cron.go
+++ forgejo/admin_cron.go
@@ -10,17 +10,9 @@ package forgejo
 
 import (
 	"fmt"
-	"time"
-)
 
-// CronTask represents a Cron task
-type CronTask struct {
-	Name      string    `json:"name"`
-	Schedule  string    `json:"schedule"`
-	Next      time.Time `json:"next"`
-	Prev      time.Time `json:"prev"`
-	ExecTimes int64     `json:"exec_times"`
-}
+	"codeberg.org/mvdkleijn/forgejo-sdk/forgejo/v2/models"
+)
 
 // ListCronTaskOptions list options for ListCronTasks
 type ListCronTaskOptions struct {
@@ -28,12 +20,12 @@ type ListCronTaskOptions struct {
 }
 
 // ListCronTasks list available cron tasks
-func (c *Client) ListCronTasks(opt ListCronTaskOptions) ([]*CronTask, *Response, error) {
+func (c *Client) ListCronTasks(opt ListCronTaskOptions) ([]*models.Cron, *Response, error) {
 	if err := c.checkServerVersionGreaterThanOrEqual(version1_13_0); err != nil {
 		return nil, nil, err
 	}
 	opt.setDefaults()
-	ct := make([]*CronTask, 0, opt.PageSize)
+	ct := make([]*models.Cron, 0, opt.PageSize)
 	resp, err := c.getParsedResponse("GET", fmt.Sprintf("/admin/cron?%s", opt.getURLQuery().Encode()), jsonHeader, nil, &ct)
 	return ct, resp, err
 }
